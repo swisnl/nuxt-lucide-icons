aaa79404b7480ab1b87b889d2b9ea4a0c5e2bdac
Jasper Zonneveld <jasper@swis.nl>
2023-06-01 11:59:38 +0200

    fix: resolve path to lucide-vue-next
    The dependency is resolved relative to the location of this module, so that package managers like pnpm without shamefully hoisting, or yarn with Plug'n'play enabled, also work.

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
-import { defineNuxtModule, addComponent } from '@nuxt/kit';
+import { addComponent, createResolver, defineNuxtModule } from '@nuxt/kit';
+import { join } from 'pathe';
 import * as icons from 'lucide-vue-next';
 
 export interface LucideModuleOptions {
@@ -18,7 +19,14 @@ export default defineNuxtModule<LucideModuleOptions>({
     namePrefix: 'Lucide',
   },
 
-  setup(options: LucideModuleOptions) {
+  async setup(options: LucideModuleOptions) {
+    // Resolve path to lucide-vue-next.
+    // The dependency is resolved relative to the location of this file, so that package managers like pnpm
+    // without shamefully hoisting, or yarn with Plug'n'play enabled, also work.
+    const { resolvePath } = createResolver(import.meta.url)
+    const entrypoint = await resolvePath('lucide-vue-next') // node_modules/lucide-vue-next/dist/cjs/lucide-vue-next.js
+    const root = join(entrypoint, '../../..') // node_modules/lucide-vue-next
+
     Object.keys(icons)
       .filter((icon) => icon !== 'default')
       .filter((icon) => !icon.startsWith('Lucide'))
@@ -27,7 +35,7 @@ export default defineNuxtModule<LucideModuleOptions>({
         addComponent({
           name: options.namePrefix + icon,
           export: icon,
-          filePath: 'lucide-vue-next',
+          filePath: root,
         })
       );
   },
